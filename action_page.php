f04d7475104b94c3fdc8796c48c3db4cefe6d99c
gourvii <88524018+gourvii@users.noreply.github.com>
2021-08-06 23:17:48 +0530

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,12 +2,53 @@
 <html>
     <body>
         <?php
-            if(isset($_POST['submit'])) {
+    if(isset($_POST['submit'])) {
 
-               $username = $_POST['name'];
-               echo "Username: ".$username. "<br/>"
-               
-            }        
-        ?>
+        $username = $_POST['name'];
+        echo "Username: ".$username. "<br/>"
+
+        $email = $_POST['email']
+        echo "Email id: ".$email. "<br/>"
+
+        $email = $_POST['email']
+        echo "Email id: ".$email. "<br/>"
+
+        $phoneno = $_POST['phone']
+        echo "Phone no: ".$phoneno. "<br/>"
+
+        $AGE = $_POST['age']
+        echo "Age: ".$AGE. "<br/>"
+
+        $DOB = $_POST['dob']
+        echo "Birthdate: ".$DOB. "<br/>"
+
+        echo "Languages studied: "
+        $checkbox1 = $_POST['c']
+        echo .$checkbox1. ", "
+        $checkbox2 = $_POST['c++']
+        echo .$checkbox2. ", "
+        $checkbox3 = $_POST['py']
+        echo .$checkbox3. ", "
+        $checkbox4 = $_POST['java']
+        echo .$checkbox4. ", "
+        $checkbox5 = $_POST['javas']
+        echo .$checkbox5. ", "
+        $checkbox6 = $_POST['html']
+        echo .$checkbox6. ", "
+
+        echo "<br/>"
+        $Training = $_POST['training']
+        echo "Training taken: ".$Training. "<br/>"
+
+        $Gender = $_POST['gender']
+        echo "Gender: " .$Gender. "<br/>"
+
+        //for file
+
+        $passw = $_POST['password']
+        echo "Password: " .$passw. "<br/>"
+
+    }        
+?>
     </body>
 </html>
